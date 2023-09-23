c2392d91ec30e5eef8f7c56c9fcf7166ec4b7d32
Gry4hon <77369398+Gry4hon@users.noreply.github.com>
2023-09-22 22:38:37 -0700

    We are so close!
    I just need to figure out how to properly validate a proper cheeseburger decker

--- CheeseBurgerCorollaryOne.cpp
+++ CheeseBurgerCorollaryOne.cpp
@@ -1,25 +1,33 @@
 #include <iostream>
 #include <vector>
+#include <thread>
+#include <map>
 
 using std::cout;
 using std::isspace;
+using std::map;
 using std::stoi;
 using std::string;
+using std::thread;
 using std::vector;
 
 string input = {"7 1 1 3 0 2 4 5 5 1 0 1 1 1 1 2 97 1 99 97 1 100 "};
 vector<int> listOfCases = {};
+vector<string> listOfAnswers = {};
+int numberOfCases = 0;
 
-void ConvertInput(){
+void ConvertInput()
+{
     string numberToAdd;
-    cout<< "Length of string: " << input[43] << "\n";
-     for (int i = 0; i < input.length(); i++)
+
+    for (int i = 0; i < input.length(); i++)
     {
         if (!isspace(input[i]))
         {
             numberToAdd += input[i];
         }
-        else{
+        else
+        {
             int temp = stoi(numberToAdd);
             listOfCases.push_back(temp);
             numberToAdd = "";
@@ -27,24 +35,110 @@ void ConvertInput(){
     }
 }
 
+bool TestDecker(int ingredients[], int numberOfDecker)
+{
+    bool canBeDecker = true;
 
+    int numberOfBuns = ingredients[0];
+    int numberOfCheese = ingredients[1];
+    int numberOfPatties = ingredients[2];
+
+    for (int i = 0; i < numberOfDecker; i++)
+    {
+        if (i <= 0)
+        {
+            numberOfBuns -= 1;
+            numberOfCheese -= 1;
+            numberOfPatties -= 1;
+            numberOfBuns -= 1;
+        }
+        else if (i > 0)
+        {
+            numberOfCheese -= 1;
+            numberOfPatties -= 1;
+            numberOfBuns -= 1;
+
+            if (numberOfBuns <= 0)
+        {
+            cout<< "Made it here" << "\n";
+            canBeDecker = false;    
+            break;
+        }
+        }
+    }
+
+    return canBeDecker;
+}
+
+void TestTheCases(vector<int> cases)
+{
+
+    //[0] is buns [1] is cheese [2] is patties
+
+    int listOfIngredients[] = {0, 0, 0};
+    for (int s = 0; s < cases[0]; s++)
+    {
+        listOfIngredients[0] += 2;
+        listOfIngredients[1] += 1;
+        listOfIngredients[2] += 1;
+    }
+
+    for (int d = 0; d < cases[1]; d++)
+    {
+        listOfIngredients[0] += 2;
+        listOfIngredients[1] += 2;
+        listOfIngredients[2] += 2;
+    }
+
+    // cout << "Number of Ingredients: " << "\n";
+    // cout << "Buns: " << listOfIngredients[0] << "\n";
+    // cout << "Cheese: " << listOfIngredients[1] << "\n";
+    // cout << "Patties: " << listOfIngredients[2] << "\n";
+    // cout << "\n";
+
+    bool isPossibleDecker = TestDecker(listOfIngredients, cases[0]);
+
+    if (isPossibleDecker)
+    {
+        listOfAnswers.push_back("Yes");
+    }
+    else
+    {
+        listOfAnswers.push_back("No");
+    }
+}
+
+void ConvertListToTestCases()
+{
+    vector<int> testCases = {};
+
+    for (int i = 1; i < listOfCases.size(); i++)
+    {
+
+        if (i % 3 != 0)
+        {
+            testCases.push_back(listOfCases[i]);
+        }
+        else
+        {
+            testCases.push_back(listOfCases[i]);
+            TestTheCases(testCases);
+            numberOfCases++;
+            testCases.clear();
+        }
+    }
+}
 
 int main()
 {
 
     ConvertInput();
-   
+    ConvertListToTestCases();
 
-    for (int i = 1; i < listOfCases.size(); i++)
+    for (int i = 1; i <= numberOfCases; i++)
     {
-        
-        if(i % 3 != 0){
-            cout<< "Item in list: " << listOfCases[i] << " ";
-        }else{
-            cout<< "Item in list: " << listOfCases[i] << "\n";
-        }
+        cout << "Case #" << i << ": " << listOfAnswers[i - 1] << "\n";
     }
-    
 
     return 0;
 }
