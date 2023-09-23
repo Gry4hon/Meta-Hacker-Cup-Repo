e7be63db68e7634f51125087306ba03a882f36d8
Gry4hon <77369398+Gry4hon@users.noreply.github.com>
2023-09-23 16:52:00 -0700

    Final Submission for problem 1A
    Just the Changes from last night during the submission :)

--- CheeseBurgerCorollaryOne.cpp
+++ CheeseBurgerCorollaryOne.cpp
@@ -11,14 +11,15 @@ using std::string;
 using std::thread;
 using std::vector;
 
-string input = {"7 1 1 3 0 2 4 5 5 1 0 1 1 1 1 2 97 1 99 97 1 100 "};
-vector<int> listOfCases = {};
+string input = "40 97 1 99 1 1 2 88 99 37 78 95 78 14 29 22 70 16 86 90 61 18 95 29 33 4 98 23 3 18 73 14 52 10 97 1 100 44 87 73 48 7 24 45 68 27 95 2 83 69 59 26 80 60 87 100 100 1 100 12 73 69 10 27 24 100 92 0 2 4 95 35 54 9 1 74 80 9 18 5 5 1 33 89 88 64 71 56 5 40 27 91 25 96 91 45 24 27 99 25 1 1 100 96 100 52 1 1 3 0 1 1 29 57 87 73 11 11 65 58 100";
+vector<int>
+    listOfCases = {};
 vector<string> listOfAnswers = {};
-int numberOfCases = 0;
 
 void ConvertInput()
 {
     string numberToAdd;
+    input += " ";
 
     for (int i = 0; i < input.length(); i++)
     {
@@ -43,34 +44,52 @@ bool TestDecker(int ingredients[], int numberOfDecker)
     int numberOfCheese = ingredients[1];
     int numberOfPatties = ingredients[2];
 
-    for (int i = 0; i < numberOfDecker; i++)
+    cout << "\n";
+    cout << "# of decker: " << numberOfDecker << "\n";
+    cout << "# of buns before: " << numberOfBuns << "\n";
+    cout << "# of cheese before: " << numberOfCheese << "\n";
+    cout << "# of patties before: " << numberOfPatties << "\n";
+
+    for (int i = 0; i < numberOfDecker - 1; i++)
     {
-        if (i <= 0)
+
+        if (i == 0)
         {
             numberOfBuns -= 1;
             numberOfCheese -= 1;
             numberOfPatties -= 1;
             numberOfBuns -= 1;
         }
-        else if (i > 0)
-        {
-            numberOfCheese -= 1;
-            numberOfPatties -= 1;
-            numberOfBuns -= 1;
 
-            if (numberOfBuns <= 0)
+        numberOfCheese -= 1;
+        numberOfPatties -= 1;
+        numberOfBuns -= 1;
+
+        if (numberOfBuns <= 0 && numberOfCheese > 0 && numberOfPatties > 0)
         {
-            cout<< "Made it here" << "\n";
-            canBeDecker = false;    
+            cout << "Made it here"
+                 << "\n";
+            canBeDecker = false;
             break;
         }
+        else if (numberOfCheese < 0 || numberOfPatties < 0)
+        {
+            cout << "Made it here"
+                 << "\n";
+            canBeDecker = false;
+            break;
         }
     }
+
+    cout << "# of buns after: " << numberOfBuns << "\n";
+    cout << "# of cheese after: " << numberOfCheese << "\n";
+    cout << "# of patties after: " << numberOfPatties << "\n";
+    cout << "\n";
 
     return canBeDecker;
 }
 
-void TestTheCases(vector<int> cases)
+void TestTheCase(vector<int> cases)
 {
 
     //[0] is buns [1] is cheese [2] is patties
@@ -90,21 +109,15 @@ void TestTheCases(vector<int> cases)
         listOfIngredients[2] += 2;
     }
 
-    // cout << "Number of Ingredients: " << "\n";
-    // cout << "Buns: " << listOfIngredients[0] << "\n";
-    // cout << "Cheese: " << listOfIngredients[1] << "\n";
-    // cout << "Patties: " << listOfIngredients[2] << "\n";
-    // cout << "\n";
-
-    bool isPossibleDecker = TestDecker(listOfIngredients, cases[0]);
+    bool isPossibleDecker = TestDecker(listOfIngredients, cases[2]);
 
     if (isPossibleDecker)
     {
-        listOfAnswers.push_back("Yes");
+        listOfAnswers.push_back("YES");
     }
     else
     {
-        listOfAnswers.push_back("No");
+        listOfAnswers.push_back("NO");
     }
 }
 
@@ -122,8 +135,7 @@ void ConvertListToTestCases()
         else
         {
             testCases.push_back(listOfCases[i]);
-            TestTheCases(testCases);
-            numberOfCases++;
+            TestTheCase(testCases);
             testCases.clear();
         }
     }
@@ -135,7 +147,7 @@ int main()
     ConvertInput();
     ConvertListToTestCases();
 
-    for (int i = 1; i <= numberOfCases; i++)
+    for (int i = 1; i <= listOfCases[0]; i++)
     {
         cout << "Case #" << i << ": " << listOfAnswers[i - 1] << "\n";
     }
